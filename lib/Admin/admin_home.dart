b3be442299b627134442b5551b95f87d633b7978
rahulmistry <r26mistry@gmail.com>
2024-10-17 11:20:24 +0530

    api set login user and register and admin site

---
+++ lib/Admin/admin_home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../pages/login.dart';
+
+class AdminHome extends StatefulWidget {
+  const AdminHome({super.key});
+
+  @override
+  State<AdminHome> createState() => _AdminHomeState();
+}
+
+class _AdminHomeState extends State<AdminHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Get.offAll(SignInPage());
+            },
+            icon: Icon(Icons.logout_rounded),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Text('Admin Home'),
+        ),
+      ),
+    );
+  }
+}
